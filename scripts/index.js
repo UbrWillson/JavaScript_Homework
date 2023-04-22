241748748da41a1e0f5a1f40b3570e539bda6a2d
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-04-23 00:25:26 +0300

    5432412

--- scripts/index.js
+++ scripts/index.js
@@ -38,3 +38,73 @@
 // document.write(show_arr())
 
 // 2.	Написати функцію, яка приймає масив чисел та повертає суму всіх чисел, які є парними. Використати методи масивів для фільтрування елементів та обчислення суми.
+
+// const arr_num = [7, 34, 62, 2, 21, 14, 88, 77]
+
+// function show_arr(){
+//     arr_num.sort();
+    
+//     let summ = 0;
+//     for(let i=0; i<arr_num.length;i++){
+//         if(arr_num[i]%2==0){
+//             summ+=arr_num[i];
+//             console.log("Це число парне "+arr_num[i]);
+            
+            
+//         }
+        
+//     }
+//     console.log(summ);
+
+
+
+
+
+// }
+// document.write(show_arr())
+
+// 3.	Написати функцію, яка приймає масив строк та повертає масив тих же строк, але з видаленими пробілами з початку та кінця. Використати методи масивів для зміни елементів
+// let arr_num = [" first element", "second elemnt", "third element "]
+// let arr_new =[]
+// function show_arr(){
+//     for(let i =0; i<arr_num.length;i++){
+//        let str = arr_num[i].trim();
+//        arr_new.push(str)
+//     }
+//     console.log(arr_new)
+    
+    
+// }
+// console.log(show_arr())
+// 1.	Написати функцію, яка приймає число та виводить таблицю множення на це число від 1 до 10. Використати цикл for для створення таблиці.
+// const arr_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// function show_arr(){
+//     let question_num = prompt("Введіть число")
+//     for(let i = 0; i<arr_num.length; i++){
+                      
+//             let result = question_num*arr_num[i]
+//             console.log(result)
+//         }
+
+
+//     }
+
+
+
+// console.log(show_arr())
+// 2.	Написати функцію, яка приймає масив чисел та повертає масив, який містить тільки унікальні числа. Використати цикл for для перевірки на унікальність та методи масивів для створення нового масиву.
+// let arr_num = [2,2,2,3,3,3,1,2,3,4,5,6]
+// function uni(arr) {
+//     let result = [];
+  
+//     for (let str of arr) {
+//       if (!result.includes(str)) {
+//         result.push(str);
+//       }
+//     }
+    
+//     return result;
+//   }
+//   console.log(uni(arr_num))
+//   3.	Написати функцію, яка приймає масив строк та повертає найдовшу строку з масиву. Використати цикл for для перебору елементів та методи масивів для порівняння довжин строк.
