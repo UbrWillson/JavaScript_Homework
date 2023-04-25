ea7d6c4119c52c19ecc37d8a652b13088c26057a
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-04-25 15:20:27 +0300

    132432цй53465435

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,3 @@
-
 // 1.	*Написати функцію, яка повертає значення гіпотенузи прямокутного трикутника за заданими довжинами його катетів. Використати методи об'єкту Math для обчислення квадратних коренів та піднесення до степеня.
 // let katet_first = 6;
 // let katet_second = 8;
@@ -9,12 +8,8 @@
 //     let triangle_sum = Math.sqrt(katet_first + katet_second)
 //     return (triangle_sum)
 
-
-
 // }
 // console.log (triangle_func())
-
-
 
 // 2.	*Написати функцію, яка випадковим чином повертає число від 0 до 1 з заданою точністю десяткового запису (наприклад, з точністю до 2 знаків після коми). Використати методи об'єкту Math для генерації випадкового числа та округлення значення.
 
@@ -32,8 +27,6 @@
 //     arr_letters.reverse()
 //     return (arr_letters)
 
-
-
 // }
 // document.write(show_arr())
 
@@ -43,22 +36,17 @@
 
 // function show_arr(){
 //     arr_num.sort();
-    
+
 //     let summ = 0;
 //     for(let i=0; i<arr_num.length;i++){
 //         if(arr_num[i]%2==0){
 //             summ+=arr_num[i];
 //             console.log("Це число парне "+arr_num[i]);
-            
-            
+
 //         }
-        
+
 //     }
 //     console.log(summ);
-
-
-
-
 
 // }
 // document.write(show_arr())
@@ -72,8 +60,7 @@
 //        arr_new.push(str)
 //     }
 //     console.log(arr_new)
-    
-    
+
 // }
 // console.log(show_arr())
 // 1.	Написати функцію, яка приймає число та виводить таблицю множення на це число від 1 до 10. Використати цикл for для створення таблиці.
@@ -82,28 +69,25 @@
 // function show_arr(){
 //     let question_num = prompt("Введіть число")
 //     for(let i = 0; i<arr_num.length; i++){
-                      
+
 //             let result = question_num*arr_num[i]
 //             console.log(result)
 //         }
 
-
 //     }
-
-
 
 // console.log(show_arr())
 // 2.	Написати функцію, яка приймає масив чисел та повертає масив, який містить тільки унікальні числа. Використати цикл for для перевірки на унікальність та методи масивів для створення нового масиву.
 // let arr_num = [2,2,2,3,3,3,1,2,3,4,5,6]
 // function uni(arr) {
 //     let result = [];
-  
+
 //     for (let str of arr) {
 //       if (!result.includes(str)) {
 //         result.push(str);
 //       }
 //     }
-    
+
 //     return result;
 //   }
 //   console.log(uni(arr_num))
@@ -116,7 +100,7 @@
 //         if(thelongeststring.length<arr_sentences[i].length){
 //              thelongeststring = arr_sentences[i];
 //         }
-       
+
 //     }
 //     return thelongeststring;
 // }
@@ -130,7 +114,7 @@
 //     for (let i = 0; i<arr_numbers.length; i++){
 //        if(arr_numbers[i]**2 > new_arr){
 //         new_arr = arr_numbers[i] ** 2;
-//        } 
+//        }
 //     }
 //   return new_arr;
 // }
@@ -148,23 +132,18 @@
 // }
 // console.log(show_verbswithout_caps())
 // 6.	Написати функцію, яка приймає масив чисел та повертає середнє значення цих чисел. Використати цикл for для обчислення суми та методи масивів для обчислення кількості елементів.
-// const arr_numbers = [2, 6, 21, 76]
-// function Sum_method(total, num){
-// return total + num
+// const arr_numbers = [2, 6, 21, 76];
+// function Sum_method(total, num) {
+//   return total + num;
 // }
 
-// function arr_num_sum(){
-// let middle_num = 0;
-// for (let i = 0; i<arr_numbers.length; i++){
-// let sum = arr_numbers.reduce(Sum_method);
-// let arr_length = arr_numbers.length;
-// middle_num = sum / arr_length;
-
+// function arr_num_sum() {
+//   let middle_num = 0;
+//   for (let i = 0; i < arr_numbers.length; i++) {
+//     let sum = arr_numbers.reduce(Sum_method);
+//     let arr_length = arr_numbers.length;
+//     middle_num = sum / arr_length;
+//   }
+//   return middle_num;
 // }
-// return middle_num;
-
-
-
-
-// }
-// console.log(arr_num_sum())
+// console.log(arr_num_sum());
