4c0dc945e837beb80a671bcc964aec2cb435f668
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-05-02 20:49:08 +0300

    updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,54 @@
+// 1.	TASK => {
+//     SubTask1: “Створіть об'єкт, який представляє вашу улюблену тварину. Додайте властивості, такі як ім'я, вид, вік та характеристики.”,
+//     Subtask2: “Створіть функцію, яка виводить на екран всі властивості об'єкта, який ви створили в попередньому завданні.”,
+//     SubTask3: “Створіть функцію, яка змінює значення властивості вашого об'єкта.”
+//     };
+    // 
+    // let animal_descr = [
+    //     {id:1,type:"cat",name:"jack",age:12,speed:"120 km per second"}
+    // ]
+    // function show_animal(){
+    //     return animal_descr
+        
+    // }
+    // console.log(show_animal())
+//     function speed_fix(){
+//         let animal_descr = [
+//             {id:1,type:"cat",name:"jack",age:12,speed:"120 km per second"}
+//         ]
+// animal_descr.speed = "10 km per second"
+// return animal_descr
+//     }
 
+//     console.log(speed_fix())
+// 2.	TASK => {
+//     SubTask1:  “Створіть два об'єкти, які представляють дві різні машини. Включіть властивості, такі як марка, модель, рік випуску та характеристики. Порівняйте ці два об'єкти та виведіть на екран результат порівняння.” ,
+//     SubTask2: “Створіть функцію, яка видаляє властивість з вашого об'єкта.”
+//     };
+
+// let car_1 = [
+//     {car_brand:"mercedes",model:"GLS",year_of_develop:2016, horse_power:340, type_of_fuel:"diesel"}
+// ]
+
+// let car_2 = [
+//     {car_brand:"bmw",model:"x6",year_of_develop:2016, horse_power:355, type_of_fuel:"diesel"}
+// ]
+
+// function show_car_aspects(){
+//     let new_car_obj = []
+//     for(let i = 0; i<car_1.length; i++){
+//         for(let j= 0; j<car_2.length; j++){
+//             if(car_1.car_brand === car_2.car_brand
+//                 && car_1[i].car_model === car_2[j].model
+//                 && car_1[i].year_of_develop === car_2[j].year_of_develop
+//                 && car_1[i].horse_power === car_2[j].horse_power
+//                 && car_1[i].type_of_fuel === car_2[j].type_of_fuel
+//                 ){
+//                     new_car_obj.push(car_1[i])
+//                 }
+//         }
+//     }
+    
+//     return new_car_obj
+// }
+// console.log(show_car_aspects())
