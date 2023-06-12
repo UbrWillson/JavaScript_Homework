fa92563923c6dadfaaadfaf80e14599624f4f70d
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-06-13 02:13:02 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,21 @@
 
+
+
+
+
+    document.getElementById("button").addEventListener('click', function funcBtn(start){
+        console.log('btn is working');
+        let time = 2;
+        start++
+
+       
+
+        setTimeout(() => {
+            
+            alert("повідомлення вивелось через 5 секунд", start);
+            
+        }, 5000);
+       
+        
+    });
+   
