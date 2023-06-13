694c1637d7abb2d0e1d735e653da21aa0b05a957
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-06-13 03:42:24 +0300

    fix for last task is needed

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,85 @@
 
 
+//1
 
 
-
-    document.getElementById("button").addEventListener('click', function funcBtn(start){
-        console.log('btn is working');
-        let time = 2;
-        start++
+    // document.getElementById("button").addEventListener('click', function funcBtn(start){
+    //     console.log('btn is working');
+    //     let time = 2;
+    //     start++
 
        
 
-        setTimeout(() => {
+    //     setTimeout(() => {
             
-            alert("повідомлення вивелось через 5 секунд", start);
+    //         alert("повідомлення вивелось через 5 секунд", start);
             
-        }, 5000);
+    //     }, 5000);
        
         
-    });
+    // });
+
+
+
+//2
+
+    // document.getElementById("button").addEventListener('click', function funcBtn(start){
+    //     console.log('btn is working');
+    //     let time = 2;
+    //     start++
+
+       
+
+    //     setTimeout(() => {
+            
+    //         alert("повідомлення вивелось через 10 секунд", start);
+    //         let massage = console.log('повідомлення отримано')
+    //     }, 10000);
+       
+        
+    // });
+
+
+//3
+
+    // document.getElementById("button").addEventListener('click', function funcBtn(){
+    //     console.log('btn is working');
+
+    //     setInterval(() => {
+    //         let d1 = new Date();
+    //         alert(d1.toTimeString());
+            
+    //     }, 5000);
+
+    // })
+
+//4
+
+//     document.getElementById("button").addEventListener('click', function funcBtn(){
+//     console.log('btn is working');
+
+//     setInterval(() => {
+//         let d1 = new Date();
+//         alert(d1.toDateString());
+        
+//     }, 5000);
+
+// })
+
+//5
+
+    document.getElementById("button").addEventListener('click', function funcBtn(){
+    console.log('btn is working');
+    dayjs.extend(dayOfYear);
+
+    let today = dayjs().dayOfYear();
+        console.log(today)
+    let overallDays = 365
+    let summ = overallDays - today;
+    console.log(summ)
+    
+
+})
+
+
    
