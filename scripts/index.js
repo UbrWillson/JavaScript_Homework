b6a2071524240668de03fbbcf9cfadf29f0850a7
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-06-16 03:56:50 +0300

    not finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,38 @@
+// class Product {
+//     constructor (name, price) {
+//         this.name = name;
+//         this.price = price;
+//     }
 
+//     discount(){
+        
+//         let discount = 30
+//         console.log(`До цього продукту примінилась знижка - 20грн, тобто ціна: ${this.price - discount}`)
+//     }
+
+   
+    
+// }
+// let myProduct = new Product('bike', 270)
+
+// console.log(myProduct.discount());
+
+
+//2
+// class Rectangle {
+//     constructor(width, height){
+//         this.width = width;
+//         this.height = height;
+//     }
+
+//     sMath(){
+//         console.log(this.width * this.height / 2)
+//     }
+
+
+// }
+
+// let myRectangle = new Rectangle(10, 4)
+// myRectangle.sMath();
+
+//3
