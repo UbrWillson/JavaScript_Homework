2bb52cc4b0a8c1efca385849c139bca1e84f1d4e
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-08-03 19:48:34 +0300

    gallery

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
 
+document.querySelectorAll(".galleryWrap img").forEach(img =>{
+    img.onclick = () => {
+        document.querySelector('.pop-up').style.display = 'block';
+        document.querySelector('.pop-up img').src = img.getAttribute('src')
+    };
+
+    
+});
+
+
+
+btnClose = document.querySelector('.pop-up span');
+
+btnClose.onclick = () => {
+    document.querySelector('.pop-up').style.display = 'none';
+}
