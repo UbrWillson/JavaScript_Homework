3e94aa1075354b622d00686455b22e7a67e0080c
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-08-05 19:29:05 +0300

    carousel finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,21 @@
+const doc = document;
+
+let numberForFunc = 1;
+
+
+const btnStart = doc.querySelector('.slideShowStart')
+
+function slideShowProcess(){
+
+    doc.querySelector('#radio' + numberForFunc).checked = true;
+    numberForFunc++;
+    if(numberForFunc > 4){
+        numberForFunc = 1;
+    }
+}
+
+btnStart.onclick = function slideshow() {
+    console.log('btn is working')
+    setInterval(slideShowProcess, 5000);
+}
 
