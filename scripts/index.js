0381bfe5418a2380d74a11e6f7e6138685b3dc72
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-08-06 16:04:34 +0300

    finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,64 @@
 
+const doc = document;
+const canvas = doc.querySelector('#canvas')
+const ctx = canvas.getContext('2d');
+
+
+// drawings
+//head
+ctx.fillStyle = 'purple'
+ctx.fillRect(400, 50, 200, 200)
+
+
+//eyes
+ctx.fillStyle = 'yellow'
+ctx.fillRect(425, 100, 50, 50)
+ctx.fillStyle = 'yellow'
+ctx.fillRect(525, 100, 50, 50)
+
+ctx.fillStyle = 'black'
+ctx.fillRect(445, 120, 10, 10)
+ctx.fillStyle = 'black'
+ctx.fillRect(545, 120, 10, 10)
+
+
+//mouth
+ctx.fillStyle = 'red'
+ctx.fillRect(450, 175, 100, 30)
+
+ctx.fillStyle = 'black'
+ctx.fillRect(450, 190, 100, 5)
+
+
+//body
+ctx.fillStyle = 'purple'
+ctx.fillRect(325, 225, 350, 500)
+
+
+//arms
+ctx.fillStyle = 'grey'
+ctx.fillRect(275, 275, 75, 250)
+ctx.fillStyle = 'pink'
+ctx.fillRect(275, 225, 125, 75)
+
+ctx.fillStyle = 'grey'
+ctx.fillRect(650, 275, 75, 250)
+ctx.fillStyle = 'pink'
+ctx.fillRect(600, 225, 125, 75)
+
+
+//legs
+
+ctx.fillStyle = 'grey'
+ctx.fillRect(325, 650, 75, 250)
+ctx.fillStyle = 'pink'
+ctx.fillRect(325, 650, 125, 75)
+
+
+ctx.fillStyle = 'grey'
+ctx.fillRect(600, 650, 75, 250)
+ctx.fillStyle = 'pink'
+ctx.fillRect(550, 650, 125, 75)
+
+
+
