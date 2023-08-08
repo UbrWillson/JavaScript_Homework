16443cce6fe5e90a04529809753d01b279f52b7e
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-08-08 23:02:55 +0300

    1223

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,34 @@
 
+const products = [
+    { name: "Product 1", price: 10.99, image: "product1.jpg" },
+    { name: "Product 2", price: 19.99, image: "product2.jpg" },
+    // Додайте інші товари
+  ];
+  
+  function createProductCard(product) {
+    const card = document.createElement("div");
+    card.classList.add("product-card"); // Додайте стилі за потреби
+  
+    const image = document.createElement("img");
+    image.src = product.image;
+    image.alt = product.name;
+    card.appendChild(image);
+  
+    const name = document.createElement("h3");
+    name.textContent = product.name;
+    card.appendChild(name);
+  
+    const price = document.createElement("p");
+    price.textContent = `$${product.price.toFixed(2)}`;
+    card.appendChild(price);
+  
+    return card;
+  }
+  
+  // Отримання контейнера і додавання карток товарів
+  const productContainer = document.getElementById("product-container");
+  
+  products.forEach(product => {
+    const card = createProductCard(product);
+    productContainer.appendChild(card);
+  });
