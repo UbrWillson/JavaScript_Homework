d3ad7bdc46e905382b80ba609b988171c84e33d6
Vlad Savenko <114161862+UbrWillson@users.noreply.github.com>
2023-08-10 23:49:27 +0300

    55454353

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,15 @@
 
 const products = [
-    { name: "Product 1", price: 10.99, image: "product1.jpg" },
+    { name: "Product 1", price: 10.99, image: "/img/laptop.jpg" },
     { name: "Product 2", price: 19.99, image: "product2.jpg" },
+    { name: "Product 3", price: 102.99, image: "product1.jpg" },
+    { name: "Product 4", price: 120.99, image: "product1.jpg" },
+    { name: "Product 5", price: 15130.99, image: "product1.jpg" },
+    { name: "Product 6", price: 14210.99, image: "product1.jpg" },
+    { name: "Product 7", price: 16450.99, image: "product1.jpg" },
+    { name: "Product 8", price: 109.99, image: "product1.jpg" },
+    { name: "Product 9", price: 1097.99, image: "product1.jpg" },
+    { name: "Product 10", price: 1065.99, image: "product1.jpg" },
     
   ];
   
@@ -12,6 +20,7 @@ const products = [
     const image = document.createElement("img");
     image.src = product.image;
     image.alt = product.name;
+    image.classList.add('product-img')
     card.appendChild(image);
   
     const name = document.createElement("h3");
